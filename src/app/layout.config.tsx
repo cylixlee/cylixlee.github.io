1f9b112a4ca116332bedccccb929426cdbadeaa2
Cylix Lee <cylix.lee@outlook.com>
2025-05-10 20:05:40 +0800

    Now the project structure is minimal.

--- src/app/layout.config.tsx
+++ src/app/layout.config.tsx
@@ -9,31 +9,6 @@ import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
  */
 export const baseOptions: BaseLayoutProps = {
   nav: {
-    title: (
-      <>
-        <svg
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-label="Logo"
-        >
-          <circle cx={12} cy={12} r={12} fill="currentColor" />
-        </svg>
-        My App
-      </>
-    ),
+    title: <>CYLIX</>,
   },
-  links: [
-    {
-      text: "Documentation",
-      url: "/docs",
-      active: "nested-url",
-
-      // This seems broken?
-      //
-      // When I set this to "menu", the "Documentation" link appears ONLY in the menu. However, when I set it to "nav",
-      // the "Documentation" link appears in both the menu and the nav, just like the "all" option.
-      on: "nav",
-    },
-  ],
 };
