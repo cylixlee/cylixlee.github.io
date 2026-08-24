2c1ea74a321dda7d5c3646621860006af2591300
Cylix Lee <cylix.lee@outlook.com>
2026-08-24 16:38:34 +0800

    feat: Refined CSS and add first post.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,21 +6,10 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
-				},
-			],
+			title: 'DeepStack',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/cylixlee' }],
+			customCss: ["./src/styles/custom.css"],
+			pagination: false,
 		}),
 	],
 });
---
+++ src/content/docs/welcome.md
@@ -0,0 +1,7 @@
+---
+title: 欢迎
+---
+
+欢迎来到 DeepStack，[Cylix Lee](https://github.com/cylixlee)的个人站点。
+
+请在左侧查看文档及博客。
