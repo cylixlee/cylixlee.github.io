1c5fe2b36f1fb308998e01a06a7d7e890d14d3d2
Cylix Lee <cylix.lee@outlook.com>
2026-08-24 18:00:04 +0800

    feat: Change caption.

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/cylixlee' }],
 			sidebar: [
 				{ label: "欢迎", slug: "welcome" },
-				{ label: "WSL", items: [{ autogenerate: { directory: "wsl" } }] },
+				{ label: "WSL2", items: [{ autogenerate: { directory: "wsl2" } }] },
 				{ label: "Windows Terminal", items: [{ autogenerate: { directory: "windows-terminal" } }] },
 				{ label: "MSYS2", items: [{ autogenerate: { directory: "msys2" } }] },
 			],
